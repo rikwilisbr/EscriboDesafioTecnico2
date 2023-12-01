657251f66631509009df6bbb68f6a3d25cd793de
Henrique William <104221714+rikwilisbr@users.noreply.github.com>
2023-11-30 21:27:54 -0300

    some fix

--- src/utils/server.js
+++ src/utils/server.js
@@ -9,7 +9,7 @@ function CreateServer () {
   app.use('/api/singUp', require('../routes/singUp')) // register/singUp user route
   app.use('/api/singIn', require('../routes/singIn')) // login/singIn user route
   app.use('/api/getUser', require('../routes/getUser')) // search user route
-
+  app.get('/', (req, res) => res.send('Api para desafio técnico Escribo, detalhes sobre endpoints: https://github.com/rikwilisbr/EscriboDesafioTecnico2/blob/main/README.md'))
   return app
 }
 
